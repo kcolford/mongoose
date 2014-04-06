43b80723558a08ff8b8d6f7f265704d82089786b
Kieran Colford <colfordk@gmail.com>
2014-04-06 17:19:14 -0400

    Added enhanced rules to the cfg.mk file

--- cfg.mk
+++ cfg.mk
@@ -1,3 +1,7 @@
+FILES_IN_VC = $(shell ./build-aux/vc-list-files)
+GENERATED_FILES_IN_VC = bootstrap COPYING
+WRITTEN_FILES = $(filter-out $(GENERATED_FILES_IN_VC) cfg.mk, $(FILES_IN_VC))
+
 man: $(srcdir)/src/compiler.c $(srcdir)/.version
 	$(MAKE) -C man
 	man -l $(top_srcdir)/man/compiler.1
@@ -5,4 +9,14 @@ man: $(srcdir)/src/compiler.c $(srcdir)/.version
 copyright:
 	@env UPDATE_COPYRIGHT_HOLDER="Kieran Colford" $(MAKE) update-copyright
 
-.PHONY: copyright man
+todo-list:
+	@grep 'TODO' $(WRITTEN_FILES) | sed -e 's/:[ \t]*/: /'
+	@grep 'FIXME' $(WRITTEN_FILES) | sed -e 's/:[ \t]*/: /'
+	@grep 'XXX' $(WRITTEN_FILES) | sed -e 's/:[ \t]*/: /'
+
+count:
+	@echo
+	@echo 'Line Counts'
+	@wc -l $(WRITTEN_FILES) cfg.mk
+
+.PHONY: copyright count man todo-list
