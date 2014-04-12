b00c66de611b706b449fcee0d5b8737537179f68
Kieran Colford <colfordk@gmail.com>
2014-04-12 17:45:51 -0400

    Comments added to header file

--- src/compiler.h
+++ src/compiler.h
@@ -30,30 +30,79 @@ along with Compiler; see the file COPYING.  If not see
 
 #include "gettext.h"
 
+/* Special macros for making it easier to mark strings for
+   translation. */
 #define _(s) gettext (s)
 #define N_(s) gettext_noop (s)
 
-extern FILE *yyin;
-extern FILE *outfile;
+extern FILE *yyin;		/* The input stream for the lexer. */
+extern FILE *outfile;		/* The output stream for the gen_code
+				   routine. */
 
-extern char *file_name;
-extern int lineno;
+extern char *file_name;		/* The current file name as determined
+				   by the lexer.  This is always
+				   either NULL or a dynamically
+				   allocated string. */
+extern int lineno;		/* The current line number as
+				   determined by the lexer. */
 
-extern int optimize;
-extern int debug;
+extern int optimize;		/* A flag describing the optimization
+				   levels that each phase must adhere
+				   to. */
+extern int debug;		/* A flag that if true says that all
+				   assembly will be echoed to
+				   stdout. */
 
+/* **************************************************************** */
+/* These are the compilation passes that manipulate the AST and verify
+   its correctness.  They all signal an error through their return
+   values so that the parser can identify where the problem occured.
+   A return value of 0 means that it successfully completed the pass,
+   while any other return value indicates error. */
+
+/* The code generation routine. */
 extern int gen_code (struct ast *);
-extern int optimizer (struct ast **);
-extern int dealias (struct ast **);
-extern int semantic (struct ast *);
 
+/* The optimizer. */
+extern int optimizer (struct ast **);
+
+/* The pass that translates all aliases into what they really mean. */
+extern int dealias (struct ast **);
+
+/* The pass that verifies the integrity of the AST and that it
+   satisfies the semantics of the C language. */
+extern int semantic (struct ast *); 
+
+/* This runs all the above routines in order and collects their return
+   values. */
 extern int run_compilation_passes (struct ast **);
 
+/* **************************************************************** */
+/* These are the general utility functions that provide things that
+   the C library doesn't. */
+
+/* This concatenates two dynamically allocated strings and frees its
+   arguments. */
 extern char *my_strcat (char *, char *);
+
+/* This creates a unique string to act as a place holder when one
+   isn't already provided. */
 extern char *place_holder (void);
+
+/* This is a printf like function that returns a dynamically allocated
+   string based on the format specifier. */
 extern char *my_printf (const char *, ...);
+
+/* This creates an appropriate file name that can be used as a
+   temporary file. */
 extern char *tmpfile_name (void);
+
+/* This is a routine that forks the calling process and then calls
+   exec to run another program (while the original program waits for
+   it to return). */
 extern int safe_system (const char **);
+
+/* This concatenates two lists of ASTs. */
 extern struct ast *ast_cat (struct ast *, struct ast *);
 
 #endif
