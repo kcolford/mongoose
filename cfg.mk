2adb84df0ba557502df7f00357d75932a7a27a8a
Kieran Colford <colfordk@gmail.com>
2014-07-11 16:55:24 -0400

    cfg.mk: removed instances of COMPILER_OPTS
    COMPILER_OPTS is only defined in cfg.mk yet this file should never be
    distributed along with a tarball and so any tests that would use this
    shouldn't work.

--- cfg.mk
+++ cfg.mk
@@ -2,9 +2,6 @@ FILES_IN_VC = $(shell ./build-aux/vc-list-files)
 GENERATED_FILES_IN_VC = bootstrap COPYING doc.cfg.in fix-cleanup.patch
 WRITTEN_FILES = $(filter-out $(GENERATED_FILES_IN_VC) cfg.mk, $(FILES_IN_VC))
 
-COMPILER_OPTS =
-export COMPILER_OPTS
-
 config_h_header = "config\.h"
 update-copyright-env = UPDATE_COPYRIGHT_HOLDER="Kieran Colford"
 local-checks-to-skip = sc_trailing_blank sc_vulnerable_makefile_CVE-2012-3386
